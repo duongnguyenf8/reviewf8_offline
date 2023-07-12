3adcfe1ac3349000b5448fa6d74b58579d6cfc07
duongnguyenf8 <nguyenduong@fullstack.edu.vn>
2023-07-13 01:55:51 +0700

    update code

--- assets/js/pages.js
+++ assets/js/pages.js
@@ -19,40 +19,6 @@ window.onload = () => {
     tagA[i].setAttribute("target", "_blank");
   }
 
-  // if href is a image, open in this tab, fullscreen, add a close button
-  for (var i = 0; i < tagA.length; i++) {
-    const href = tagA[i].getAttribute("href");
-    if (
-      href.includes(".png") ||
-      href.includes(".jpg") ||
-      href.includes(".jpeg") ||
-      href.includes(".gif") ||
-      href.includes(".svg") ||
-      href.includes(".webp") ||
-      href.includes(".bmp") ||
-      href.includes(".ico") ||
-      href.includes(".tiff") ||
-      href.includes(".tif") ||
-      href.includes(".jfif") ||
-      href.includes(".pjpeg") ||
-      href.includes(".pjp") ||
-      href.includes(".avif") ||
-      href.includes(".apng") ||
-      href.includes(".jfif-tbnl") ||
-      href.includes(".svgz") ||
-      href.includes(".ico") ||
-      href.includes(".cur") ||
-      href.includes(".ani") ||
-      href.includes(".jpe") ||
-      href.includes(".avifs") ||
-      href.includes(".avif")
-    ) {
-      tagA[i].setAttribute("target", "_self");
-      tagA[i].setAttribute("onclick", "openFullscreen()");
-      tagA[i].setAttribute("oncontextmenu", "return false;");
-    }
-  }
-
   backBtn.className = "back-btn";
   backBtn.innerText = "Back";
   backBtn.onclick = () => {
